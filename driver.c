74dd8f4ae1c034da566f29e9203b154acd5d52be
Harshan Baskar <harshan01@gmail.com>
2020-10-22 23:38:01 +0530

    Dry test tokenise

--- driver.c
+++ driver.c
@@ -1,6 +1,10 @@
 #include "grammar.h"
+#include "token.h"
 
 void main() {
     grammar G = (struct production_rule*)malloc(sizeof(struct production_rule) * NUM_RULES);
     readGrammar("grammar.txt", G);
+
+    tokenStream* s = (tokenStream*)malloc(sizeof(tokenStream));
+    tokeniseSourcecode("sourcecode.txt", s);
 }
